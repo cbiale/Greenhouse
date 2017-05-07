91eafad442c4caf30fcc602f7ba1890c697c98ad
ray <a25237780a@gmail.com>
2017-05-07 16:38:51 +0800

    第一次完整 測試 1

--- config.py
+++ config.py
@@ -5,12 +5,12 @@ import socket
 
 HOSTNAME = socket.gethostname()
 
-DB_HOST ="10.74.23.153"
+DB_HOST =""
 #在Class  A之中，有4個網域無法使用，分別為0.0.0.0/8、
 #10.0.0.0/8、100.64.0.0/10、127.0.0.0/8。0.0.0.0/8
 #是個特殊的網路區段，這個位址表示所有、任何、預設的意思。
 #如果在路由表的來源IP為只填入0.0.0.0，代表的就是無論資料封包從哪裡過來
 #一律套用此路由規則。
 DB_PORT = 28015
-DB_NAME = "irving"
-PROJECT_TABLE = 'observations'
+DB_NAME = ""
+PROJECT_TABLE = ''
